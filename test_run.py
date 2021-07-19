42c0727743319d73190433a65df8a0ac52adf0f2
root <root@DESKTOP-IHT95E4.localdomain>
2021-07-18 17:30:33 -0700

    y

--- test_run.py
+++ test_run.py
@@ -12,8 +12,8 @@ import astropy.units as u
 import populations as p
 
 p.set_detector("Design")
-nsbh_population = p.Population([1.4, 0.5, 2, 1, 3, 5, 4], 'one', False, selection=True, m1_nospin = True)
-pop_samples = nsbh_population.get_population(100, True)
+nsbh_population = p.Population([1.4, 0.5, 2, 1, 3, 5, 4], 'nsbh_one', False, selection=True, m1_nospin = True)
+pop_samples = nsbh_population.get_population(40, True)
 samples, likes = nsbh_population.infer(pop_samples, 20000, save_to = './full_test_run.h5')
 np.savetxt('./full_test_run_samples.txt', samples)
 np.savetxt('./full_test_run_likes.txt', likes)
