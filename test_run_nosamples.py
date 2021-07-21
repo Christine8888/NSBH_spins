c9484330c5558a2f8a66cd5b0a934315381e55fb
Christine Ye <christine.ye@ldas-grid.ligo.caltech.edu>
2021-07-20 20:07:43 -0700

    test

--- test_run_nosamples.py
+++ test_run_nosamples.py
@@ -12,7 +12,7 @@ import astropy.units as u
 import populations as p
 
 p.set_detector("APlus")
-nsbh_population = p.Population([1.4, 0.5, 2, 1, 3, 5, 4], 'nsbh_one', False, selection=True, m1_nospin = True)
+nsbh_population = p.Population([1.4, 0.5, 2, 1, 3, 5, 4], 'nsbh_one', False, selection=True, m1_nospin = True, spinning=True, spin_params=[0.8, 1])
 nsbh_population.set_injection_spins(p.injection_set)
 pop_samples = nsbh_population.get_population(100, False)
 samples, likes = nsbh_population.infer(pop_samples, 10000, save_to = './nosample_test_run.h5')
