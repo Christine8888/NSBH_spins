bbf955f395eb15feeef6c0c57b3463d575e9c24d
Christine Ye <christine.ye@ldas-grid.ligo.caltech.edu>
2021-08-05 08:46:11 -0700

    scripts  Please enter the commit message for your changes. Lines starting

---
+++ .nfs0000000000003095000b630c
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Running TOVs"
+conda activate igwn-py39
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2 
--- mTOV_1c.py
+++ mTOV_1c.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('~/NSBH_spins')
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
@@ -22,6 +24,7 @@ parser.add_argument("--max_jjkep", type=float, default=1.0)
 parser.add_argument("--spin_slope", type=float, default=0.0)
 parser.add_argument("--bh_min", type=float, default=5.0)
 parser.add_argument("--bh_slope", type=float, default=2.0)
+parser.add_argument("--folder", type=str, default="mTOV_convergence")
 
 args = parser.parse_args()
 
@@ -34,20 +37,21 @@ max_jjkep = args.max_jjkep
 spin_slope = args.spin_slope
 bh_min = args.bh_min
 bh_slope = args.bh_slope
+folder = args.folder
 
-
-nsbh_population = p.Population([0.63, 1.35, 0.07, 1.85, 0.35, mtov_True, 1, 3, bh_min, bh_slope], 'nsbh', vary_slope=False, selection=True, m1_nospin = True, spinning=True, spin_params=[max_jjkep, spin_slope])
+nsbh_population = p.Population([1.5, 0.5, mtov_True, 1, 3, bh_min, bh_slope], 'nsbh_one', vary_slope=False, selection=True, m1_nospin = True, spinning=True, spin_params=[max_jjkep, spin_slope])
 nsbh_population.set_injection_spins(p.injection_set)
 
 pop_samples = nsbh_population.get_population(event_counts[0], True)
-
-fixed = {"a":0.63, "mu_1": 1.35, "sigma_1":0.07, "mu_2": 1.85, "sigma_2":0.35, "m_TOV":[mtov_True,1.7,3.2], "bh_min": bh_min, "bh_slope": bh_slope, "max_jjkep": max_jjkep, "spin_slope": spin_slope}
-
+fixed = {"mu": 1.5, "sigma":0.5, "m_TOV":[mtov_True,1.7,3.2], "bh_min": bh_min, "bh_slope": bh_slope, "max_jjkep": max_jjkep, "spin_slope": spin_slope}
 for i in range(5):
-    samples, likes = nsbh_population.infer(pop_samples, 2000, mult=True, save_to = None,fixed=fixed)
-    np.savetxt('../mTOV_convergence/{}_mTOV_{}_run_{}_2component.txt'.format(detector, mtov_True, str(event_counts[i])), samples)
-    np.savetxt('../mTOV_convergence/{}_mTOV_{}_run_{}_2component_likes.txt'.format(detector, mtov_True, str(event_counts[i])), likes)
+    samples, likes = nsbh_population.infer(pop_samples, 2000, save_to=None, fixed=fixed, mult=True)
+    np.savetxt('../{}/{}_mTOV_{}_run_{}.txt'.format(folder, detector, mtov_True, str(event_counts[i])), samples)
+    np.savetxt('../{}/{}_mTOV_{}_run_{}_likes.txt'.format(folder, detector, mtov_True, str(event_counts[i])), likes)
     if i != 4:
-    	new_samples = nsbh_population.get_population(event_counts[i+1]-event_counts[i], True)
-    	pop_samples = np.vstack([pop_samples, new_samples])
+        new_samples = nsbh_population.get_population(event_counts[i+1]-event_counts[i], True)
+        pop_samples = np.vstack([pop_samples, new_samples])
     print(pop_samples.shape)
+
+# pop_samples = pop_samples.reshape((180000,4))
+# np.savetxt('./mTOV_{}_run_{}_popsamples'.format(mtov_True, str(event_counts[i])), pop_samples)
