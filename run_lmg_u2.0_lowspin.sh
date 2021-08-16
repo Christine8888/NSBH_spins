30a3ab5942e48fe3d4112d5ea426a3acf75a19cb
Christine Ye <christine.ye@ldas-grid.ligo.caltech.edu>
2021-08-15 22:28:43 -0700

    lmg scripts

---
+++ run_lmg_u2.0_lowspin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python LMG_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 --bh_min 5 --spin_slope 2 --folder LMG_convergence_lowspin
+#python LMG_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.5 --bh_min 2.5  --spin_slope 2 --folder LMG_convergence_lowspin
+python LMG_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.0 --bh_min 2.0  --spin_slope 2 --folder LMG_convergence_lowspin
+python LMG_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.0 --bh_min 2.41  --spin_slope 2 --folder LMG_convergence_lowspin
+python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --bh_min 5 --spin_slope 2 --folder LMG_convergence_lowspin
+#python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.5 --bh_min 2.5 --spin_slope 2 --folder LMG_convergence_lowspin
+python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.0 --spin_slope 2 --folder LMG_convergence_lowspin
+python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.41 --spin_slope 2 --folder LMG_convergence_lowspin
+
