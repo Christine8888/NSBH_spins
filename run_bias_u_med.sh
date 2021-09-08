e6e38a3bc5a7955d863e20c2a1cdc9546328c108
Christine Ye <christine.ye@ldas-grid.ligo.caltech.edu>
2021-09-07 22:13:31 -0700

    files i Please enter the commit message for your changes. Lines starting

---
+++ run_bias_u_med.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python mTOV_bias_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_bias_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_bias_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_bias_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin
---
+++ run_slope_2c_med.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python mTOV_slope_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --slope 0.4
+python mTOV_slope_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --slope 0.4
+python mTOV_slope_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --slope 0.4
+python mTOV_slope_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --slope 0.4
