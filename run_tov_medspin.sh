0cf1edfe7d4f98a4f81d933426613bd09cdacc49
root <root@DESKTOP-IHT95E4.localdomain>
2021-09-07 22:44:03 -0700

    medspin tov

---
+++ run_tov_medspin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Running TOVs"
+conda activate igwn-py39
+python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --spin_slope 0 --max_jjkep 0.5 --folder medspinlow
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 --spin_slope 0 --max_jjkep 0.5 --folder medspin
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin 
