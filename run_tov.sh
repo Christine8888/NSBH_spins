f9cc9da88ddfd9e5edaaf620339ecb066f8d3065
root <root@DESKTOP-IHT95E4.localdomain>
2021-09-07 22:47:03 -0700

    fixing injections

--- run_tov.sh
+++ run_tov.sh
@@ -2,19 +2,19 @@
 echo "Running TOVs"
 conda activate igwn-py39
 
-python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 
-python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2 
-python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 
-python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 
+python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2
+python mTOV_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2
+python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2
+python mTOV_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2
 
-python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 
-python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2 
-python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 
+python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2
+python mTOV_2c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2
+python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2
 python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2
 
-python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 
-python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 
-python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2
+python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2
 python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2
 
 
@@ -31,4 +31,4 @@ python mTOV_2c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5
 python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --spin_slope 2 --folder lowspin
 python mTOV_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --spin_slope 2 --folder lowspin
 python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2 --spin_slope 2 --folder lowspin
-python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin 
+python mTOV_u.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin
