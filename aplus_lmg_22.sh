fa39b6fefc026b42cac8401232bcd83fe3e7eb67
root <root@DESKTOP-IHT95E4.localdomain>
2021-09-13 16:07:50 -0700

    runs reset yuck

--- aplus_lmg_22.sh
+++ aplus_lmg_22.sh
@@ -2,7 +2,7 @@
 echo "Running LMG"
 conda activate igwn-py39
 #python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 5 --spin_slope 0
-python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 0
+#python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 0
 #python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.65 --spin_slope 0
 python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 5 --spin_slope 2 --folder LMG_convergence_lowspin
 python LMG.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 2 --folder LMG_convergence_lowspin
