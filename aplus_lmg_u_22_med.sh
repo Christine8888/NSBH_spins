0892a84827b646f0940ae3158716796f73f22e20
Christine Ye <christine.ye@ldas-grid.ligo.caltech.edu>
2021-09-14 22:45:48 -0700

    sh

--- aplus_lmg_u_22_med.sh
+++ aplus_lmg_u_22_med.sh
@@ -7,6 +7,6 @@ conda activate igwn-py39
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 5 --spin_slope 2 --folder LMG_convergence_lowspin
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 2 --folder LMG_convergence_lowspin
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.65 --spin_slope 2 --folder LMG_convergence_lowspin
-python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 5 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
-python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
+#python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 5 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
+#python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.2 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
 python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2 --bh_min 2.65 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
--- aplus_lmg_u_2_med.sh
+++ aplus_lmg_u_2_med.sh
@@ -8,6 +8,6 @@ conda activate igwn-py39
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --bh_min 5 --spin_slope 2 --folder LMG_convergence_lowspin
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.0 --spin_slope 2 --folder LMG_convergence_lowspin
 #python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.41 --spin_slope 2 --folder LMG_convergence_lowspin
-python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --bh_min 5 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
-python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.0 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
+#python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2 --bh_min 5 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
+#python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.0 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
 python LMG_u.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.0 --bh_min 2.41 --spin_slope 0 --max_jjkep 0.5 --folder LMG_convergence_medspin
