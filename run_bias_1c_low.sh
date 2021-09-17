821fcdc317537c28676d0289a918d1eb4b766dcf
root <root@DESKTOP-IHT95E4.localdomain>
2021-09-16 20:30:13 -0700

    changes

--- run_bias_1c_low.sh
+++ run_bias_1c_low.sh
@@ -2,7 +2,7 @@
 echo "Running TOVs"
 conda activate igwn-py39
 
-python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 2 --folder lowspin --free
-python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin --free
-python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 2 --folder lowspin --free
-python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin --free
+python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 2 --folder lowspin_99pc --free
+python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin_99pc --free
+python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 2 --folder lowspin_99pc --free
+python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 2 --folder lowspin_99pc --free
--- run_bias_1c_med.sh
+++ run_bias_1c_med.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --free
-python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --free
-python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --free
-python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin --free
+python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin_99pc --free
+python mTOV_bias_1c.py --detector "APlus" --event_min 30 --event_max 150 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin_99pc --free
+python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2  --spin_slope 0 --max_jjkep 0.5 --folder medspin_99pc --free
+python mTOV_bias_1c.py --detector "Design" --event_min 10 --event_max 50 --n_events 5 --mtov_true 2.2  --spin_slope 0 --max_jjkep 0.5 --folder medspin_99pc --free
