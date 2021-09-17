66b2d55136c5a5d658e3bcf2490ddee70784d43e
root <root@DESKTOP-IHT95E4.localdomain>
2021-09-16 20:27:21 -0700

    all the 1c runs

--- LMG_1c.py
+++ LMG_1c.py
@@ -25,8 +25,9 @@ parser.add_argument("--max_jjkep", type=float, default=1.0)
 parser.add_argument("--spin_slope", type=float, default=0.0)
 parser.add_argument("--bh_min", type=float, default=5.0)
 parser.add_argument("--bh_slope", type=float, default=2.0)
-parser.add_argument("--folder", type=str, default="mTOV_convergence")
-parser.add_argument("--free", action='store_true')
+parser.add_argument("--folder", type=str, default="LMG_convergence")
+parser.add_argument("--free", action="store_true")
+
 args = parser.parse_args()
 
 event_counts = np.linspace(args.event_min, args.event_max, args.n_events, dtype='int')
@@ -39,22 +40,24 @@ spin_slope = args.spin_slope
 bh_min = args.bh_min
 bh_slope = args.bh_slope
 folder = args.folder
-free = args.free
 
-nsbh_population = p.Population([1.5, 100, mtov_True, 1, 3, bh_min, bh_slope], 'nsbh_one', vary_slope=False, selection=True, m1_nospin = True, spinning=True, spin_params=[max_jjkep, spin_slope], ignore_spin=True)
+nsbh_population = p.Population([1.5, 100, mtov_True, 1, 3, bh_min, bh_slope], 'nsbh_one', vary_slope=False, selection=True, m1_nospin = True, spinning=True, spin_params=[max_jjkep, spin_slope])
 nsbh_population.set_injection_spins(p.injection_set)
 
 pop_samples = nsbh_population.get_population(event_counts[0], True)
 
-N=2000
-fixed = {"mu": 1.5, "sigma":100, "m_TOV":[mtov_True+0.3,1.8,3.5], "bh_min": bh_min, "bh_slope": bh_slope, "max_jjkep": max_jjkep, "spin_slope": spin_slope}
+if args.free:
+    N = 5000
+    fixed = {"m_TOV":[mtov_True,1.8,3.5], "bh_min":[bh_min, 1.5, bh_min+2], "bh_slope": bh_slope, "max_jjkep": max_jjkep, "spin_slope": spin_slope}
+else:
+    N = 3000
+    fixed = {"mu": 1.5, "sigma": 0.5, "m_TOV":[mtov_True,1.8,3.5], "bh_min":[bh_min, 1.5, bh_min+2], "bh_slope": bh_slope, "max_jjkep": max_jjkep, "spin_slope": spin_slope}
 
 for i in range(5):
     samples, likes = nsbh_population.infer(pop_samples, N, mult=True, save_to = None,fixed=fixed)
-    np.savetxt('../{}/{}_mTOV_{}_run_{}_u_bias.txt'.format(folder, detector, mtov_True, str(event_counts[i])), samples)
-    np.savetxt('../{}/{}_mTOV_{}_run_{}_u_bias_likes.txt'.format(folder, detector, mtov_True, str(event_counts[i])), likes)
+    np.savetxt('../{}/{}_mTOV_{}_bhmin_{}_{}_1c.txt'.format(folder, detector, mtov_True, bh_min, str(event_counts[i])), samples)
+    np.savetxt('../{}/{}_mTOV_{}_bhmin_{}_{}_1c_likes.txt'.format(folder, detector, mtov_True, bh_min, str(event_counts[i])), likes)
     if i != 4:
     	new_samples = nsbh_population.get_population(event_counts[i+1]-event_counts[i], True)
     	pop_samples = np.vstack([pop_samples, new_samples])
     print(pop_samples.shape)
-print('../{}/{}_mTOV_{}_run_{}_u_bias_likes.txt'.format(folder, detector, mtov_True, str(event_counts[i])))
