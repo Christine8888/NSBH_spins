e8fb482ae01ab87ff1bf59a4c87d79bf44763e8c
root <root@DESKTOP-IHT95E4.localdomain>
2021-10-08 16:14:57 -0700

    ispins

--- run_real_data_1c.sh
+++ run_real_data_1c.sh
@@ -2,35 +2,35 @@
 echo "Running real data"
 conda activate igwn-py39
 
-python run_real_data.py --gw190426 --gw190814 --type direct --name wide --poptype 1c
-python run_real_data.py --gw190426 --gw190814 --type chieff --name wide --poptype 1c
-python run_real_data.py --gw190426 --gw190814 --type pos_chieff --name wide --poptype 1c
+python run_real_data.py --gw190426 --gw190814 --type direct --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190426 --gw190814 --type chieff --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190426 --gw190814 --type pos_chieff --name widespin --poptype 1c --freespin
 
-python run_real_data.py --gw190426 --type direct --name wide --poptype 1c
-python run_real_data.py --gw190426 --type chieff --name wide --poptype 1c
-python run_real_data.py --gw190426 --type pos_chieff --name wide --poptype 1c
+python run_real_data.py --gw190426 --type direct --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190426 --type chieff --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190426 --type pos_chieff --name widespin --poptype 1c --freespin
 
-python run_real_data.py --gw190814 --type direct --name wide --poptype 1c
-python run_real_data.py --gw190814 --type chieff --name wide --poptype 1c
-python run_real_data.py --gw190814 --type pos_chieff --name wide --poptype 1c
+python run_real_data.py --gw190814 --type direct --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190814 --type chieff --name widespin --poptype 1c --freespin
+python run_real_data.py --gw190814 --type pos_chieff --name widespin --poptype 1c --freespin
 
-python run_real_data.py --type direct --name wide --poptype 1c
-python run_real_data.py --type chieff --name wide --poptype 1c
-python run_real_data.py --type pos_chieff --name wide --poptype 1c
+python run_real_data.py --type direct --name widespin --poptype 1c --freespin
+python run_real_data.py --type chieff --name widespin --poptype 1c --freespin
+python run_real_data.py --type pos_chieff --name widespin --poptype 1c --freespin
 
 
-#python run_real_data.py --gw190426 --gw190814 --type direct --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190426 --gw190814 --type chieff --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190426 --gw190814 --type pos_chieff --name wide --beta 0 --poptype 1c
+#python run_real_data.py --gw190426 --gw190814 --type direct --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190426 --gw190814 --type chieff --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190426 --gw190814 --type pos_chieff --name widespin --beta 0 --poptype 1c --freespin
 
-#python run_real_data.py --gw190426 --type direct --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190426 --type chieff --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190426 --type pos_chieff --name wide --beta 0 --poptype 1c
+#python run_real_data.py --gw190426 --type direct --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190426 --type chieff --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190426 --type pos_chieff --name widespin --beta 0 --poptype 1c --freespin
 
-#python run_real_data.py --gw190814 --type direct --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190814 --type chieff --name wide --beta 0 --poptype 1c
-#python run_real_data.py --gw190814 --type pos_chieff --name wide --beta 0 --poptype 1c
+#python run_real_data.py --gw190814 --type direct --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190814 --type chieff --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --gw190814 --type pos_chieff --name widespin --beta 0 --poptype 1c --freespin
 
-#python run_real_data.py --type direct --name wide --beta 0 --poptype 1c
-#python run_real_data.py --type chieff --name wide --beta 0 --poptype 1c
-#python run_real_data.py --type pos_chieff --name wide --beta 0 --poptype 1c
+#python run_real_data.py --type direct --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --type chieff --name widespin --beta 0 --poptype 1c --freespin
+#python run_real_data.py --type pos_chieff --name widespin --beta 0 --poptype 1c --freespin
