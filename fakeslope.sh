2f6883d7222629071dfba2858649c37562e4db8b
root <root@DESKTOP-IHT95E4.localdomain>
2021-11-27 23:28:13 -0800

    slope

--- fakeslope.sh
+++ fakeslope.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python run_real_data.py --gw190426 --gw190814 --type fake --name slope --poptype 1c --freespin --beta 0 --slope
-python run_real_data.py --gw190426 --type fake --name slope --poptype 1c --freespin --beta 0 --slope
-python run_real_data.py --gw190814 --type fake --name slope --poptype 1c --freespin --beta 0 --slope
-python run_real_data.py --type fake --name slope --poptype 1c --freespin --beta 0 --slope
+python run_real_data.py --slope --gw190426 --gw190814 --type fake --name slope --poptype 1c --freespin --beta 0
+python run_real_data.py --slope --gw190426 --type fake --name slope --poptype 1c --freespin --beta 0
+python run_real_data.py --slope --gw190814 --type fake --name slope --poptype 1c --freespin --beta 0
+python run_real_data.py --slope --type fake --name slope --poptype 1c --freespin --beta 0
 
-python run_real_data.py --gw190426 --gw190814 --type fake --name slope --poptype u --freespin --beta 0 --slope
-python run_real_data.py --gw190426 --type fake --name slope --poptype u --freespin --beta 0 --slope
-python run_real_data.py --gw190814 --type fake --name slope --poptype u --freespin --beta 0 --slope
-python run_real_data.py --type fake --name slope --poptype u --freespin --beta 0 --slope
+python run_real_data.py --slope --gw190426 --gw190814 --type fake --name slope --poptype u --freespin --beta 0
+python run_real_data.py --slope --gw190426 --type fake --name slope --poptype u --freespin --beta 0
+python run_real_data.py --slope --gw190814 --type fake --name slope --poptype u --freespin --beta 0
+python run_real_data.py --slope --type fake --name slope --poptype u --freespin --beta 0
 
-python run_real_data.py --gw190426 --gw190814 --type fake --name slope --poptype 2c --freespin --beta 0 --slope
-python run_real_data.py --gw190426 --type fake --name slope --poptype 2c --freespin --beta 0 --slope
-python run_real_data.py --gw190814 --type fake --name slope --poptype 2c --freespin --beta 0 --slope
-python run_real_data.py --type fake --name slope --poptype 2c --freespin --beta 0 --slope
+python run_real_data.py --slope --gw190426 --gw190814 --type fake --name slope --poptype 2c --freespin --beta 0
+python run_real_data.py --slope --gw190426 --type fake --name slope --poptype 2c --freespin --beta 0
+python run_real_data.py --slope --gw190814 --type fake --name slope --poptype 2c --freespin --beta 0
+python run_real_data.py --slope --type fake --name slope --poptype 2c --freespin --beta 0
