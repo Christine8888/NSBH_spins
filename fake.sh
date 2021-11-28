5388e3024dd425893e6a211d79a5ef55f453239f
root <root@DESKTOP-IHT95E4.localdomain>
2021-11-27 22:50:08 -0800

    fake

---
+++ fake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#python run_real_data.py --gw190426 --gw190814 --type fake --name fake --poptype 1c --freespin --beta 0
+#python run_real_data.py --gw190426 --type fake --name fake --poptype 1c --freespin --beta 0
+python run_real_data.py --gw190814 --type fake --name fake --poptype 1c --freespin --beta 0
+python run_real_data.py --type fake --name fake --poptype 1c --freespin --beta 0
+
+python run_real_data.py --gw190426 --gw190814 --type fake --name fake --poptype u --freespin --beta 0
+python run_real_data.py --gw190426 --type fake --name fake --poptype u --freespin --beta 0
+python run_real_data.py --gw190814 --type fake --name fake --poptype u --freespin --beta 0
+python run_real_data.py --type fake --name fake --poptype u --freespin --beta 0
